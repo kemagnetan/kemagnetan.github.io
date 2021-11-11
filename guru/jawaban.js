f621cd69c15bb7139b65ead741d97d7d44d40cba
alfian <4lfianfirdaus@gmail.com>
2021-11-11 07:02:58 +0700

    update222

--- guru/jawaban.js
+++ guru/jawaban.js
@@ -297,3 +297,35 @@ function deletetask(id) {
 
     }
 }
+
+
+//download table
+function exportTableToExcel(tabel, filename = '') {
+    var downloadLink;
+    var dataType = 'application/vnd.ms-excel';
+    var tabel = document.getElementById(tabel);
+    var tableHTML = tabel.outerHTML.replace(/ /g, '%20');
+
+    // Specify file name
+    filename = filename ? filename + '.xls' : 'excel_data.xls';
+
+    // Create download link element
+    downloadLink = document.createElement("a");
+
+    document.body.appendChild(downloadLink);
+
+    if (navigator.msSaveOrOpenBlob) {
+        var blob = new Blob(['\ufeff', tableHTML], {
+            type: dataType
+        });
+        navigator.msSaveOrOpenBlob(blob, filename);
+    } else {
+        // Create a link to the file
+        downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
+        // Setting the file name
+        downloadLink.download = filename;
+
+        //triggering the function
+        downloadLink.click();
+    }
+}
